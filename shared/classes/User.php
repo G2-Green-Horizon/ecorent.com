9c39cf700028ab3b3a1436f3ef40254a1b613b5d
Fushiguro019 <marklouievillanueva584@gmail.com>
2024-12-30 10:15:03 +0800

    Initial commit

---
+++ shared/classes/User.php
@@ -0,0 +1,14 @@
+<?php
+    class User{
+        public $firstName;
+        public $lastName;
+        public $userType;
+
+        public function __construct(){
+
+        }
+        public function rentItem(){
+
+        }
+    }
+?>
