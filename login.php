f0c1355e7144efebd39c2ff0f8dba3303e434290
denisegabrielle <dnsgbrllsuarez@gmail.com>
2025-01-08 16:00:10 +0800

    Created the login page

---
+++ login.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ecorent | Login</title>
+
+    <!-- STYLINGS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="shared/assets/css/style.css">
+</head>
+
+<body>
+
+    <section class="container">
+        <div class="row align-items-center">
+            <!-- Image Column -->
+            <div class="col-md-6 text-center">
+                <img src="shared/assets/img/picture.png" class="img-fluid" alt="Sample Picture">
+            </div>
+
+            <!-- Form Column -->
+            <div class="col-md-6 p-5 text-center">
+                <h1 class="mb-4">Welcome back</h1>
+                <form>
+                    <div class="mb-3">
+                        <input type="email" class="form-control" placeholder="Email or username" id="email">
+                    </div>
+                    <div class="mb-3">
+                        <input type="password" class="form-control" placeholder="Password" id="password">
+                    </div>
+                    <div class="mb-4 d-flex justify-content-start">
+                         <a href="">Forget password?</a>
+                    </div>
+                    <div>
+                        <button class="btnContinue  w-100" type="submit">Continue</button>
+                    </div>
+                    <div class="my-4 text-center">
+                         <a href="">Don't have an account?</a>
+                    </div>
+                    <div>
+                        <button class="btnRegister w-100" type="submit">Register</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="shared/assets/js/script.js"></script>
+
+</body>
+
+</html>
