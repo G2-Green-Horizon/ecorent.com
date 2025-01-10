1fb04aead0f944c3c0930f5332577dbbf5cfaf1a
Shane <shanekane567@gmail.com>
2025-01-11 00:27:25 +0800

    Populate Database

--- index.php
+++ index.php
@@ -1,25 +1,149 @@
-
-
 <!doctype html>
 <html lang="en">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Homepage</title>
-    
+    <title>ecorent | Homepage</title>
+
     <!-- STYLINGS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="shared/assets/css/style.css">
 </head>
 
-<body>
-    <h1>Client page</h1>
+<body id="homepage">
+
+    <div class="container my-3 p-4 rounded-5" id="carousel-container">
+        <div class="row g-2">
+            <div class="col-md-8">
+                <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0"
+                            class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="shared/assets/img/system/caro1.png" class="d-block w-100" alt="Carousel Image 1">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="shared/assets/img/system/caro1.png" class="d-block w-100" alt="Carousel Image 2">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="shared/assets/img/system/caro1.png" class="d-block w-100" alt="Carousel Image 3">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 d-flex flex-column gap-2">
+                <img src="shared/assets/img/system/static-image.png" class="w-100 rounded-5" alt="Static Image 1">
+                <img src="shared/assets/img/system/static-image.png" class="w-100 rounded-5" alt="Static Image 2">
+            </div>
+        </div>
+    </div>
+
+
+    <section class="container my-3">
+        <div class="row justify-content-between g-3">
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-auto">
+                <div class="d-flex align-items-center p-3 rounded-pill" style="background-color: #343333;">
+                    <img src="shared/assets/img/system/circle1.png" class="img-fluid me-2"
+                        style="width: 55px; height: auto;">
+                    <h5 class="mb-0">Electronics</h5>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-auto">
+                <div class="d-flex align-items-center p-3 rounded-pill" style="background-color: #343333;">
+                    <img src="shared/assets/img/system/circle1.png" class="img-fluid me-2"
+                        style="width: 55px; height: auto;">
+                    <h5 class="mb-0">Transportation</h5>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-sm-6 col-md-4 col-lg-2">
+                <div class="d-flex align-items-center p-3 rounded-pill" style="background-color: #343333;">
+                    <img src="shared/assets/img/system/circle1.png" class="img-fluid me-2"
+                        style="width: 55px; height: auto;">
+                    <h5 class="mb-0">Fashion</h5>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-sm-6 col-md-6 col-lg-2">
+                <div class="d-flex align-items-center p-3 rounded-pill" style="background-color: #343333;">
+                    <img src="shared/assets/img/system/circle1.png" class="img-fluid me-2"
+                        style="width: 55px; height: auto;">
+                    <h5 class="mb-0">Sports</h5>
+                </div>
+            </div>
+
+
+            <div class="col-12 col-sm-6 col-md-6 col-lg-2">
+                <div class="d-flex align-items-center p-3 rounded-pill" style="background-color: #343333;">
+                    <img src="shared/assets/img/system/circle1.png" class="img-fluid me-2"
+                        style="width: 55px; height: auto;">
+                    <h5 class="mb-0">Events</h5>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="container my-3">
+            <div class="row">
+                <div class="col">
+                    <div class="h2">
+                        Just for you
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="container">
+            </div>
+
+            <div class="text-center my-3">
+                <button class="btn btn-dark">
+                    SEE MORE
+                </button>
+            </div>
+        </div>
+    </section>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
     <script src="shared/assets/js/script.js"></script>
+
+    <script>
+        var itemNames = ["TrailMaster X200 Mountain", "TrailMaster X200 Mountain", "TrailMaster X200 Mountain", "TrailMaster X200 Mountain", "TrailMaster X200 Mountain", "TrailMaster X200 Mountain", "TrailMaster X200 Mountain", "TrailMaster X200 Mountain"];
+        var pics = ["bike1.png", "bike1.png", "bike1.png", "bike1.png", "bike1.png", "bike1.png", "bike1.png", "bike1.png"];
+        var prices = ["₱100", "₱100", "₱100", "₱100", "₱100", "₱100", "₱100", "₱100"];
+
+        for (var i = 0; i < itemNames.length; i++) {
+            var container = document.getElementById("container");
+            container.innerHTML += `
+        <div class="col-12 col-md-6 col-lg-4 col-xl-3">
+            <div class="card my-3 custom-card" id="card${i}">
+                <img src="shared/assets/img/system/${pics[i]}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">${itemNames[i]}</h5>
+                    <h5 class="card-text">Bike</h5>
+                    <h5 class="card-text price">${prices[i]}</h5>
+                </div>
+            </div>
+        </div>
+    `;
+        }
+    </script>
 
 </body>
 
---
+++ product-page.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>ecorent | Product Details</title>
+
+    <!-- STYLINGS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="shared/assets/css/style.css">
+</head>
+
+<body>
+    <section class="container">
+
+        <div class="row row-product-details my-4 mx-3 mx-sm-4">
+
+            <!-- Product Images Column -->
+            <div class="col-md-6 my-4">
+                <img src="shared/assets/img/p1.png" class="img-fluid img-fluid-product" alt="Product Image">
+            </div>
+
+            <div class="col-md-6 p-4">
+                <h3>TrailMaster X200 Mountain Bike</h3>
+                <hr>
+                <h4 class="price-custom-color ">₱500/day</h4>
+
+                <div class="d-flex align-items-center">
+                    <i class="bi bi-geo-alt-fill me-2"></i>
+                    <p class="mb-0">Brgy. San Antonio, Sto. Tomas, Batangas</p>
+                </div>
+
+                <span class="badge rounded-pill text-bg-secondary my-4">Pick-up</span>
+
+                <div class="d-flex align-items-center justify-content-end">
+                    <button class="button-size btn btn-outline-dark mx-3">Add to cart</button>
+                    <button class="button-size btn btn-dark">Rent now</button>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Product Desc -->
+        <div class="row row-product-details my-4 p-3 mx-3 mx-sm-4">
+
+            <div class="col my-3">
+                <h3>Product Description</h3>
+                <p class="mt-4">TrailMaster X200 Mountain Bike</p>
+                <p>Lightweight aluminum alloy frame available in 15", 17", 19", and 21" sizes, paired with a RockShox
+                    Judy Silver TK fork offering 100mm travel. </p>
+            </div>
+        </div>
+
+        <!-- Product Specifications -->
+        <div class="row row-product-details my-4 p-3 mx-3 mx-sm-4">
+
+            <div class="col my-4">
+                <h3>Product Specifications</h3>
+                <p class="mt-4">TrailMaster X200 Mountain Bike</p>
+                <p>Lightweight aluminum alloy frame available in 15", 17", 19", and 21" sizes, paired with a RockShox
+                    Judy Silver TK fork offering 100mm travel.</p>
+                <p>Features 29" wheels with double-walled aluminum rims and Maxxis Ardent 29x2.4"
+                    all-terrain tires. </p>
+                <p>
+                    Equipped with a Shimano Deore 12-speed drivetrain, Shimano hydraulic disc brakes, and ergonomic
+                    cockpit components including a 760mm handlebar and dropper post. </p>
+                <p>
+                    Weighing 13.5 kg, it includes mounts for bottle cages and racks, available in Matte Black, Electric
+                    Blue, and Forest Green, with a 5-year frame warranty.</p>
+            </div>
+        </div>
+
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <script src="shared/assets/js/script.js"></script>
+
+</body>
+
+</html>
