d790b0aa67b97a7f7034413a6f9ac4abfec774b6
ALeycano <leycano32@gmail.com>
2025-01-12 22:31:36 +0800

    404 error page updated

---
+++ 404.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EcoRent | Listings</title>
+    <link rel="icon" type="image/png" href="shared/assets/img/system/ecorent-logo-2.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="shared/assets/css/style.css">
+    <link rel="stylesheet" href="shared/assets/css/footerNav.css">
+    <link rel="stylesheet" href="shared/assets/css/404.css">
+    <link rel="stylesheet" href="shared/assets/font/font.css">
+</head>
+
+<body id="listings">
+
+    <?php include 'shared/components/navbar.php'; ?>
+
+    <div class="main-wrapper"> 
+        <main class="notfound-main">
+            <img src="admin/assets/img/logo/404.png" alt="Error 404" class="notfound-image">
+            <p class="notfound-message">The page you requested may have been moved, or it just doesn't exist.</p>
+            <a href="index.php" class="notfound-home-button">Go Back Home</a>
+        </main>
+    </div> 
+
+    <?php include 'shared/components/footer.php'; ?>
+
+</body>
+
+</html>
