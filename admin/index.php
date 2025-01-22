64ae0180a70c400af34196b4c0ae1fa5b59cd375
Jannah Ibarondo <jannah.ibarondo13@gmail.com>
2025-01-22 23:19:04 +0800

    implemented update rental status

--- admin/index.php
+++ admin/index.php
@@ -100,6 +100,24 @@ if (isset($_POST['cancelEdit'])) {
 
     header("Location: index.php#displayedItem" . $itemID);
 }
+
+// ACTIVE RENTALS STATUS
+if (isset($_POST['btnReceived'])) {
+    $rentalID = $_POST['rentalID'];
+    
+    $statusQuery = "SELECT rentalStatus FROM rentals WHERE rentalID = '$rentalID'";
+    $statusQueryResult = executeQuery($statusQuery);
+
+    while ($status = mysqli_fetch_assoc($statusQueryResult)) {
+        if ($status['rentalStatus'] === 'on rent') {
+            $handInQuery = "UPDATE rentals SET rentalStatus = 'received' WHERE rentalID = '$rentalID'";
+            executeQuery($handInQuery);
+        }
+    }
+    
+    header("Location: index.php");
+    exit();
+}
 ?>
 
 <!doctype html>
